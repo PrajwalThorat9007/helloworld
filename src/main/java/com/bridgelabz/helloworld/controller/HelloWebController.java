d8b233a730ec220febc3fb60f38d226d7313658d
Prajwal Sanjay Thorat <prajwalthorat9007@gmail.com>
2026-03-17 16:52:09 +0530

    UC2:Display Hello message in Web Page using MVC Controller and Thymeleaf view

---
+++ src/main/java/com/bridgelabz/helloworld/controller/HelloWebController.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWebController {
+
+    @GetMapping("/web")
+    public String hello(Model model) {
+
+        model.addAttribute("message", "Hello from BridgeLabz MVC");
+
+        return "hello";
+    }
+}
