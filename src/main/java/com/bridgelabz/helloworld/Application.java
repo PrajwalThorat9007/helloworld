2cb74ddc67532565ba243f7bd81756a529c93c16
Prajwal Sanjay Thorat <prajwalthorat9007@gmail.com>
2026-03-18 10:52:18 +0530

    UC4:Add Logger to log messages using SLF4J

--- src/main/java/com/bridgelabz/helloworld/Application.java
+++ src/main/java/com/bridgelabz/helloworld/Application.java
@@ -2,12 +2,21 @@ package com.bridgelabz.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
+		log.info("Spring Boot Application Starting...");
+
 		SpringApplication.run(Application.class, args);
+
+		log.info("Application Started Successfully");
+
 	}
+	private static final Logger log =
+			LoggerFactory.getLogger(Application.class);
 
 }
--- src/main/java/com/bridgelabz/helloworld/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/helloworld/controller/HelloRestController.java
@@ -1,15 +1,25 @@
 package com.bridgelabz.helloworld.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import com.bridgelabz.helloworld.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/api/hello")
+
 public class HelloRestController {
+
+    private static final Logger log =
+            LoggerFactory.getLogger(HelloRestController.class);
 
     // 1 simple
     @GetMapping
     public String sayHello() {
+
+        log.info("Hello API called");
+
         return "Hello from BridgeLabz";
     }
 
