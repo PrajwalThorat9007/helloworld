58d682797ad541dc658aadc582e36059bed0857a
Prajwal Sanjay Thorat <prajwalthorat9007@gmail.com>
2026-03-18 11:05:19 +0530

    UC7:Demonstrate Spring IoC container using ApplicationContext and getting beans from context

--- src/main/java/com/bridgelabz/helloworld/Application.java
+++ src/main/java/com/bridgelabz/helloworld/Application.java
@@ -1,25 +1,27 @@
 package com.bridgelabz.helloworld;
 
+import com.bridgelabz.helloworld.component.EmployeeBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import com.bridgelabz.helloworld.component.EmployeeBean;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
+
+		System.out.println("Before Spring Run");
+
 		ApplicationContext context =
 				SpringApplication.run(Application.class, args);
 
-		EmployeeBean emp = context.getBean(EmployeeBean.class);
+		System.out.println("After Spring Run");
+
+		EmployeeBean emp =
+				context.getBean(EmployeeBean.class);
+
+		System.out.println("Bean received from IoC");
 
 		emp.showEmployee();
-
 	}
-	private static final Logger log =
-			LoggerFactory.getLogger(Application.class);
-
 }
