b3b740f5e3a0fd4d70e1eb4a4a3972f74cedc3ba
Prajwal Sanjay Thorat <prajwalthorat9007@gmail.com>
2026-03-18 11:01:44 +0530

    UC6:Demonstrate Dependency Injection using attribute, constructor and setter using @Autowired

--- src/main/java/com/bridgelabz/helloworld/component/EmployeeBean.java
+++ src/main/java/com/bridgelabz/helloworld/component/EmployeeBean.java
@@ -9,13 +9,33 @@ public class EmployeeBean {
     private int id = 101;
     private String name = "Prajwal";
 
-    @Autowired
     private DepartmentBean departmentBean;
+
+    // ---------- Field Injection ----------
+    @Autowired
+    private DepartmentBean fieldDept;
+
+    // ---------- Constructor Injection ----------
+    @Autowired
+    public EmployeeBean(DepartmentBean departmentBean) {
+        this.departmentBean = departmentBean;
+    }
+
+    // ---------- Setter Injection ----------
+    @Autowired
+    public void setDepartmentBean(DepartmentBean departmentBean) {
+        this.departmentBean = departmentBean;
+    }
 
     public void showEmployee() {
 
         System.out.println("ID: " + id);
         System.out.println("Name: " + name);
-        System.out.println("Dept: " + departmentBean.getDeptName());
+
+        System.out.println("Dept (constructor/setter): "
+                + departmentBean.getDeptName());
+
+        System.out.println("Dept (field): "
+                + fieldDept.getDeptName());
     }
 }
